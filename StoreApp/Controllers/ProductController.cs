9068ecd017dbc19dbb925c5dfef1f061e91631a8
Mert Koç <mertkoc1981@gmail.com>
2024-05-12 20:25:00 +0300

    product controller and view created

---
+++ StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly RepositoryContext _repositoryContext;
+        
+        public ProductController(RepositoryContext context)
+        {
+            _repositoryContext = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _repositoryContext.Products.ToList();
+            return View(products);
+        }
+        public IActionResult Get(int id) {
+            Product product = _repositoryContext.Products.First(p => p.Id == id); 
+            return View(product);
+        }
+    }
+}
