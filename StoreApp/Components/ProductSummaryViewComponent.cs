ea8dd3e8643446a22a047a6c7f3ea7dd3091f09f
Mert Koç <mertkoc1981@gmail.com>
2024-05-13 15:46:52 +0300

    component structure implemented

---
+++ StoreApp/Components/ProductSummaryViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+using StoreApp.Models;
+using System.Globalization;
+
+namespace StoreApp.Components
+{
+    public class ProductSummaryViewComponent : ViewComponent
+    {
+        private readonly IServiceManager _serviceManager;
+        
+        public ProductSummaryViewComponent(IServiceManager manager)
+        {
+            _serviceManager = manager;
+        }
+        public string Invoke()
+        {
+            return _serviceManager.ProductService.GetAllProduct(false).Count().ToString();
+        }
+    }
+}
