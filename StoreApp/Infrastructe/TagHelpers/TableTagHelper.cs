2b9995a9dd96a15673b1a0fb9c833b819c4feb49
Mert Koç <mertkoc1981@gmail.com>
2024-05-18 21:14:33 +0300

    custom tag helpers added

---
+++ StoreApp/Infrastructe/TagHelpers/TableTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace StoreApp.Infrastructe.TagHelpers
+{
+    [HtmlTargetElement("table")]
+    public class TableTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            // proje de table gördüğü yerlerde boostrapin class table özelliğini ekliyor
+            output.Attributes.SetAttribute("class", "table table-hover");
+
+        }
+    }
+}
