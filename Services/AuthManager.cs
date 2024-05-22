12e6bf26ae1b75fddfef7c460904ab0b9d9e13fe
Mert Koç <mertkoc1981@gmail.com>
2024-05-22 21:47:39 +0300

    update user created in admin area

--- Services/AuthManager.cs
+++ Services/AuthManager.cs
@@ -45,5 +45,47 @@ namespace Services
         {
             return _userManager.Users.ToList();
         }
+
+        public async Task<IdentityUser> GetOneUser(string userName)
+        {
+            return await _userManager.FindByNameAsync(userName);
+
+        }
+
+        public async Task<UserDtoForUpdate> GetOneUserForUpdate(string userName)
+        {
+            var user = await GetOneUser(userName);
+            if (user is not null)
+            {
+                var userDto = _mapper.Map<UserDtoForUpdate>(user);
+                userDto.Roles = new HashSet<string>(Roles.Select(r => r.Name).ToList());
+                userDto.UserRoles = new HashSet<string>(await _userManager.GetRolesAsync(user));
+                return userDto;
+            }
+            throw new Exception("Bir hata oluştu");
+        }
+
+        public async Task Update(UserDtoForUpdate userDto)
+        {
+            var user = await GetOneUser(userDto.UserName);
+            user.PhoneNumber = userDto.PhoneNumber;
+            user.Email = userDto.Email;
+
+            if (user is not null)
+            {
+                var result = await _userManager.UpdateAsync(user);
+                if (userDto.Roles.Count > 0)
+                {
+                    var userRoles = await _userManager.GetRolesAsync(user);
+                    //güncelleme yapmadan önce user rolleri alınıyor
+                    var r1 = await _userManager.RemoveFromRolesAsync(user, userRoles);
+                    //burada yeni roller ekleniyor
+                    var r2 = await _userManager.AddToRolesAsync(user, userDto.Roles);
+
+                }
+                return;
+            }
+            throw new Exception("Kullanıcıyı güncellerken hata oluştu");
+        }
     }
 }
--- Services/Contracts/IAuthService.cs
+++ Services/Contracts/IAuthService.cs
@@ -1,6 +1,7 @@
 ﻿
 using Entities.Dtos;
 using Microsoft.AspNetCore.Identity;
+using System.Globalization;
 using System.Security.Principal;
 
 namespace Services.Contracts
@@ -10,9 +11,12 @@ namespace Services.Contracts
         IEnumerable<IdentityRole> Roles { get; }
 
         IEnumerable<IdentityUser> GetAllUsers();
+        //userDtoforUpdate deki role alanları da alabilmek için bu tanımı yazdık
+        Task<UserDtoForUpdate> GetOneUserForUpdate(string userName);
 
         Task<IdentityResult> CreateUser(UserDtoForCreation userDto);
 
-        
+        Task<IdentityUser> GetOneUser(string userName);
+        Task Update(UserDtoForUpdate userDto);
     }
 }
